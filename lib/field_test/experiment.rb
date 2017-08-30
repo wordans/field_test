af5a51636db12bb112aa51f685e6c81c6d232041
Pedro Rio <prio@teachme.site>
2017-08-30 13:17:08 +0100

    Debugging

--- lib/field_test/experiment.rb
+++ lib/field_test/experiment.rb
@@ -327,15 +327,9 @@ module FieldTest
           beta_2 =  b_weight
           beta_3 = c_weight
 
-          # alpha_1 = a[:participated]
-          # alpha_2 = b[:participated]
-          # alpha_3 = c[:participated]
-
-          # ========================
-
-          a[:conversion_value].blank? || a[:participated].nil? ? alpha_1 = nil : alpha_1 = a[:conversion_value] * a[:participated]
-          b[:conversion_value].blank? || b[:participated].nil? ? alpha_2 = nil : alpha_2 = b[:conversion_value] * b[:participated]
-          c[:conversion_value].blank? || c[:participated].nil? ? alpha_3 = nil : alpha_3 = c[:conversion_value] * c[:participated]
+          alpha_1 = a[:conversion_value]
+          alpha_2 = b[:conversion_value]
+          alpha_3 = c[:conversion_value]
 
           # TODO calculate this incrementally by caching intermediate results
           prob_winning =
